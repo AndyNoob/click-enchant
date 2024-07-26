83d71d43c8341bfd4dd7fe2bafe593a5820bd39b
AndyNoob <comfortableandy9082@gmail.com>
2024-07-26 16:54:34 -0700

    Updated plugin to require books in order to extract enchants (& cleaned up code)

--- src/main/java/comfortable_andy/click_enchanting/ClickEnchantingMain.java
+++ src/main/java/comfortable_andy/click_enchanting/ClickEnchantingMain.java
@@ -1,5 +1,6 @@
 package comfortable_andy.click_enchanting;
 
+import comfortable_andy.click_enchanting.util.EnchantUtil;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.Sound;
@@ -8,16 +9,16 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.ClickType;
-import org.bukkit.event.inventory.InventoryAction;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.EnchantmentStorageMeta;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.Map;
 
+import static comfortable_andy.click_enchanting.util.EnchantUtil.*;
+
+@SuppressWarnings("deprecation")
 public final class ClickEnchantingMain extends JavaPlugin implements Listener {
 
     @Override
@@ -25,103 +26,99 @@ public final class ClickEnchantingMain extends JavaPlugin implements Listener {
         getServer().getPluginManager().registerEvents(this, this);
     }
 
-    @SuppressWarnings("deprecation")
     @EventHandler
     public void onClick(InventoryClickEvent event) {
         final ItemStack cursor = event.getCursor();
         final ItemStack currentItem = event.getCurrentItem();
+        final boolean isCurrentItemEmpty = currentItem == null
+                || currentItem.isEmpty()
+                || currentItem.getType().isAir();
         if (cursor.getType().isAir() || cursor.getAmount() != 1) return;
         // TODO display experience
         if (cursor.getType() == Material.ENCHANTED_BOOK) {
             final EnchantmentStorageMeta meta = (EnchantmentStorageMeta) cursor.getItemMeta();
-            final Map<Enchantment, Integer> enchants = meta.getStoredEnchants();
-            if (currentItem == null
-                    || currentItem.isEmpty()
-                    || currentItem.getType().isAir()) {
-                // partition enchanting books
-                if (event.getAction() != InventoryAction.PLACE_ONE) return;
+            if (isCurrentItemEmpty) {
+                return;
+            } else if (event.getClick() == ClickType.LEFT) {
+                if (failedEnchant(event, meta)) return;
+                cursor.setAmount(0);
+            } else if (event.getClick() == ClickType.RIGHT
+                    && bookOrEnchantedBook(currentItem)) {
+                // is book && partition enchanting books
                 if (failedPartitionEnchants(event, cursor)) return;
                 else event.getWhoClicked().setItemOnCursor(cursor);
-            } else {
-                if (event.getClick() != ClickType.LEFT) return;
-                int levels = 0;
-                for (Map.Entry<Enchantment, Integer> entry : enchants.entrySet()) {
-                    final Enchantment enchant = entry.getKey();
-                    final Integer level = entry.getValue();
-                    levels += level * Math.max(1, enchant.getAnvilCost() / 2);
-                }
-                Player player = (Player) event.getWhoClicked();
-                if (player.getLevel() < levels) {
-                    player.playSound(event.getWhoClicked(), Sound.ENTITY_VILLAGER_NO, 1, 0.8f);
-                    player.sendMessage(ChatColor.RED + "Not enough experience levels! (Needed " + levels + ")");
-                    return;
-                }
-                player.giveExpLevels(-levels);
-                for (Map.Entry<Enchantment, Integer> entry : enchants.entrySet()) {
-                    final Enchantment enchant = entry.getKey();
-                    addEnchant(currentItem, enchant, entry.getValue());
-                }
-                cursor.setAmount(0);
-
             }
             if (!cursor.isEmpty()) cursor.setItemMeta(meta);
         } else if (!cursor.getEnchantments().isEmpty()) {
             if (event.getClickedInventory() == null) return;
-            if (event.getAction() != InventoryAction.PLACE_ONE) return;
+            if (event.getClick() != ClickType.RIGHT) return;
+            if (isCurrentItemEmpty || notBook(currentItem)) {
+                exitInventory((Player) event.getWhoClicked(), ChatColor.RED + "You can only extract into a book!");
+                event.setCancelled(true);
+                return;
+            }
             if (failedPartitionEnchants(event, cursor)) return;
             else event.getWhoClicked().setItemOnCursor(cursor);
         } else return;
         event.setCancelled(true);
     }
 
-    private static boolean failedPartitionEnchants(InventoryClickEvent event, ItemStack item) {
-        final EnchantmentStorageMeta meta = item.getType() == Material.ENCHANTED_BOOK ? (EnchantmentStorageMeta) item.getItemMeta() : null;
-        final Map<Enchantment, Integer> enchants = meta == null ? item.getEnchantments() : meta.getStoredEnchants();
-        if (enchants.isEmpty()) return true;
-        final Enchantment enchant = findLast(enchants);
-        if (enchant == null) return true;
-        int level = meta != null ? meta.getStoredEnchantLevel(enchant) : item.getEnchantmentLevel(enchant);
-        if (meta == null) {
-            item.removeEnchantment(enchant);
-        } else {
-            if (level <= 1) return true;
-            meta.removeStoredEnchant(enchant);
-            if (enchants.size() == 1) {
-                meta.addStoredEnchant(enchant, level -= 1, true);
-            }
-            if (meta.getStoredEnchants().isEmpty()) item.setAmount(0);
+    private static boolean failedEnchant(InventoryClickEvent event, EnchantmentStorageMeta meta) {
+        ItemStack currentItem = event.getCurrentItem();
+        if (currentItem == null) return true;
+        final Player player = (Player) event.getWhoClicked();
+        final Map<Enchantment, Integer> enchants = meta.getStoredEnchants();
+        int levels = 0;
+        for (Map.Entry<Enchantment, Integer> entry : enchants.entrySet()) {
+            final Enchantment enchant = entry.getKey();
+            final Integer level = entry.getValue();
+            levels += level * Math.max(1, enchant.getAnvilCost() / 2);
         }
-        final ItemStack book = makeBook(enchant, level);
-        event.getView().setItem(event.getRawSlot(), book);
-        if (meta != null && !item.isEmpty()) {
-            item.setItemMeta(meta);
+        if (player.getLevel() < levels) {
+            exitInventory(player, ChatColor.RED + "Not enough experience levels! (Needed " + levels + ")");
+            event.setCancelled(true);
+            return true;
         }
+        player.giveExpLevels(-levels);
+        for (Map.Entry<Enchantment, Integer> entry : enchants.entrySet()) {
+            final Enchantment enchant = entry.getKey();
+            EnchantUtil.addEnchant(currentItem, enchant, entry.getValue());
+        }
+        event.setCurrentItem(currentItem);
         return false;
     }
 
-    private static void addEnchant(ItemStack item, Enchantment enchant, int level) {
-        if (item.getType() == Material.ENCHANTED_BOOK) {
-            final EnchantmentStorageMeta meta = (EnchantmentStorageMeta) item.getItemMeta();
-            final int storedLevel = meta.getStoredEnchantLevel(enchant);
-            meta.addStoredEnchant(enchant, storedLevel == level ? storedLevel + 1 : Math.max(storedLevel, level), true);
-            item.setItemMeta(meta);
+    private static void exitInventory(Player player, String msg) {
+        player.playSound(player.getLocation(), Sound.ENTITY_VILLAGER_NO, 1, 0.8f);
+        player.sendMessage(msg);
+        player.closeInventory();
+    }
+
+    private static boolean failedPartitionEnchants(InventoryClickEvent event, ItemStack extracting) {
+        final EnchantmentStorageMeta meta = extracting.getType() == Material.ENCHANTED_BOOK ? (EnchantmentStorageMeta) extracting.getItemMeta() : null;
+        final boolean isNormalItem = meta == null;
+        final boolean isEnchantedBook = meta != null;
+        final Map<Enchantment, Integer> enchants = isNormalItem ? extracting.getEnchantments() : meta.getStoredEnchants();
+        if (enchants.isEmpty()) return true;
+        final Enchantment enchant = EnchantUtil.findLast(enchants);
+        if (enchant == null) return true;
+        int level = isEnchantedBook ? meta.getStoredEnchantLevel(enchant) : extracting.getEnchantmentLevel(enchant);
+        if (isNormalItem) {
+            extracting.removeEnchantment(enchant);
         } else {
-            final int otherLevel = item.getEnchantmentLevel(enchant);
-            item.addUnsafeEnchantment(enchant, otherLevel == level ? otherLevel + 1 : Math.max(otherLevel, level));
+            meta.removeStoredEnchant(enchant);
+            level -= 1;
+            if (level > 0 && enchants.size() == 1) {
+                meta.addStoredEnchant(enchant, level, true);
+            } else if (enchants.size() == 1) {
+                extracting.setType(Material.BOOK);
+            }
         }
+        final int newLevel = Math.max(1, level);
+        final ItemStack book = event.getCurrentItem() == null ? makeBook(enchant, newLevel) : addEnchant(event.getCurrentItem(), enchant, newLevel);
+        event.getView().setItem(event.getRawSlot(), book);
+        if (isEnchantedBook && notBook(extracting)) extracting.setItemMeta(meta);
+        return false;
     }
 
-    @NotNull
-    private static ItemStack makeBook(Enchantment enchant, int level) {
-        final ItemStack book = new ItemStack(Material.ENCHANTED_BOOK);
-        final EnchantmentStorageMeta meta = (EnchantmentStorageMeta) book.getItemMeta();
-        meta.addStoredEnchant(enchant, level, true);
-        book.setItemMeta(meta);
-        return book;
-    }
-
-    @Nullable
-    private static Enchantment findLast(Map<Enchantment, Integer> enchants) {
-        return enchants.keySet().stream().reduce((a, b) -> a).orElse(null);
-    }
 }
---
+++ src/main/java/comfortable_andy/click_enchanting/util/EnchantUtil.java
@@ -0,0 +1,55 @@
+package comfortable_andy.click_enchanting.util;
+
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.SoundCategory;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.EnchantmentStorageMeta;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Map;
+
+public class EnchantUtil {
+    public static void playEnchantEffect(Player player) {
+        player.playSound(player.getLocation(), Sound.BLOCK_ENCHANTMENT_TABLE_USE, SoundCategory.MASTER, 1, 1);
+    }
+    public static boolean notBook(ItemStack item) {
+        return item == null || item.getAmount() != 1 || item.getType() != Material.BOOK;
+    }
+
+    public static boolean bookOrEnchantedBook(ItemStack item) {
+        return !notBook(item) || item.getType() == Material.ENCHANTED_BOOK;
+    }
+
+    @SuppressWarnings("deprecation")
+    public static ItemStack addEnchant(ItemStack item, Enchantment enchant, int level) {
+        if (item.getType() == Material.BOOK) item.setType(Material.ENCHANTED_BOOK);
+        if (item.getType() == Material.ENCHANTED_BOOK) {
+            final EnchantmentStorageMeta meta = (EnchantmentStorageMeta) item.getItemMeta();
+            final int storedLevel = meta.getStoredEnchantLevel(enchant);
+            meta.addStoredEnchant(enchant, storedLevel == level ? storedLevel + 1 : Math.max(storedLevel, level), true);
+            item.setItemMeta(meta);
+        } else {
+            final int otherLevel = item.getEnchantmentLevel(enchant);
+            item.addUnsafeEnchantment(enchant, otherLevel == level ? otherLevel + 1 : Math.max(otherLevel, level));
+        }
+        return item;
+    }
+
+    @NotNull
+    public static ItemStack makeBook(Enchantment enchant, int level) {
+        final ItemStack book = new ItemStack(Material.ENCHANTED_BOOK);
+        final EnchantmentStorageMeta meta = (EnchantmentStorageMeta) book.getItemMeta();
+        meta.addStoredEnchant(enchant, level, true);
+        book.setItemMeta(meta);
+        return book;
+    }
+
+    @Nullable
+    public static Enchantment findLast(Map<Enchantment, Integer> enchants) {
+        return enchants.keySet().stream().reduce((a, b) -> a).orElse(null);
+    }
+}
